65a1a24a1af32bf0a29b7f0d69cb815de4f2689d
Midhun Xavier <midhunxavier@outlook.com>
2026-01-17 23:23:00 +0100

    Task 0.1: Mirror src and requirements in code

---
+++ code/src/estimators/__init__.py
@@ -0,0 +1,10 @@
+# Estimators subpackage
+from src.estimators.maxk_estimator import (
+    maxk_reward_estimate,
+    maxk_reward_weights,
+)
+
+__all__ = [
+    "maxk_reward_estimate",
+    "maxk_reward_weights",
+]
