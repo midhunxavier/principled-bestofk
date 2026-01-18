c4dd6369f8f3902dc195110ee0bdced8aa87486b
Midhun Xavier <midhunxavier@outlook.com>
2026-01-18 11:50:14 +0100

    Task 2.3: Implement LOO baselines

--- code/src/estimators/__init__.py
+++ code/src/estimators/__init__.py
@@ -1,12 +1,17 @@
 # Estimators subpackage
-from src.estimators.maxk_estimator import (
-    maxk_reward_estimate,
-    maxk_reward_weights,
+from src.estimators.baselines import (
+    apply_sample_loo,
+    sample_loo_baseline,
+    subloo_weights,
 )
+from src.estimators.maxk_estimator import maxk_reward_estimate, maxk_reward_weights
 from src.estimators.maxk_gradient import maxk_gradient_weights
 
 __all__ = [
+    "apply_sample_loo",
     "maxk_gradient_weights",
     "maxk_reward_estimate",
     "maxk_reward_weights",
+    "sample_loo_baseline",
+    "subloo_weights",
 ]
